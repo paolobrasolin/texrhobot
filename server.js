83f6107bdfa79b39b4a55335084a4dbc347d60b3
Paolo Brasolin <paolo.brasolin@gmail.com>
2016-09-28 03:54:22 +0200

    it works

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const MD5 = require('md5');
 const mkdirp = require('mkdirp');
 mkdirp('tex_cache');
 
+require('shelljs/global');
+
 const server = new Hapi.Server();
 server.connection({
     port: ~~process.env.PORT || 3000,
@@ -47,64 +49,15 @@ server.route({
             var yaml = YAML.stringify(req.payload);
             var hash = MD5(yaml);
 
-            var pandoc = ChildProcess.spawn('pandoc', [
-                `tex_cache/${hash}.yml`,
-                '--template=template.tex',
-                `--output=tex_cache/${hash}.tex`
-            ]);
-
-            // pandoc.stdout.on('data', (data) => {
-                // console.log(`stdout: ${data}`);
-            // });
-
-            // pandoc.stderr.on('data', (data) => {
-                // console.log(`stderr: ${data}`);
-            // });
-
-            var latex = ChildProcess.spawn('latex', [
-                `${hash}.tex`,
-            ], {
-                cwd: 'tex_cache'
-            });
-
-            // latex.stdout.on('data', (data) => {
-                // console.log(`stdout: ${data}`);
-            // });
-
-            // latex.stderr.on('data', (data) => {
-                // console.log(`stderr: ${data}`);
-            // });
-
-            var dvisvgm = ChildProcess.spawn('dvisvgm', [
-                `${hash}.dvi`,
-                '--no-fonts'
-                // '--zip'
-            ], {
-                cwd: 'tex_cache'
-            });
-
-            // dvisvgm.stdout.on('data', (data) => {
-                // console.log(`stdout: ${data}`);
-            // });
-
-            // dvisvgm.stderr.on('data', (data) => {
-                // console.log(`stderr: ${data}`);
-            // });
-
             fs.writeFile(
                 `tex_cache/${hash}.yml`,
                 `---\n${yaml}...`
             );
-            pandoc.on('close', (code) => {
-                console.log(`pandoc exited with code ${code}`);
-                    latex.on('close', (code) => {
-                        console.log(`latex exited with code ${code}`);
-                            dvisvgm.on('close', (code) => {
-                                console.log(`dvisvgm exited with code ${code}`);
-                                reply.file(`tex_cache/${hash}.svg`);
-                            });
-                    });
-            });
+
+            exec(`pandoc ${hash}.yml --template=../template.tex --output=${hash}.tex`,{cwd: 'tex_cache'});
+            exec(`latex ${hash}.tex`,{cwd: 'tex_cache'});
+            exec(`dvisvgm ${hash}.dvi --no-fonts`,{cwd: 'tex_cache'});
+            reply.file(`tex_cache/${hash}.svg`);
 
 
 
