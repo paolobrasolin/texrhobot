f6a163e8a9bad8b706c22acd49927edffdf7d963
Paolo Brasolin <paolo.brasolin@gmail.com>
2016-10-06 18:16:06 +0200

    tidy up

--- texrhobot.rb
+++ texrhobot.rb
@@ -3,9 +3,16 @@ require 'open3'
 require 'json'
 require 'yaml'
 
-CACHE_DIR = '.tex-cache'.freeze
-FileUtils.mkdir_p CACHE_DIR
-FileUtils.cp Dir['*.fmt'], CACHE_DIR
+TMP_DIR = '.cache'.freeze # cache folder
+TPL_DIR = 'templates'.freeze # templates folder
+FMT_DIR = 'formats'.freeze # formats folder
+
+def offset_line_numbers(tex_log, offset)
+  tex_log.gsub(/^l.(\d+)(.*)$/) { "l.#{$1.to_i + offset}" + $2 }
+end
+
+FileUtils.mkdir_p TMP_DIR
+FileUtils.cp Dir["#{FMT_DIR}/*.fmt"], TMP_DIR
 
 get '/' do
   send_file 'index.html'
@@ -16,18 +23,16 @@ post '/crank' do
 
   @yaml = params.to_yaml
   @hash = Digest::MD5.hexdigest(@yaml)
-  File.open("#{CACHE_DIR}/#{@hash}.yaml", 'w') do |file|
-    file.write(@yaml)
-  end
+  File.open("#{TMP_DIR}/#{@hash}.yaml", 'w').write(@yaml)
 
-  renderer = ERB.new(File.read('template.tex.erb'), 0, '<>')
-  File.write("#{CACHE_DIR}/#{@hash}.tex", renderer.result(binding))
+  renderer = ERB.new(File.read("#{TPL_DIR}/#{params['template']}.tex.erb"), 0, '<>')
+  File.write("#{TMP_DIR}/#{@hash}.tex", renderer.result(binding))
 
-  Dir.chdir CACHE_DIR do
-    _stdout, _stderr, status = Open3.capture3(
+  Dir.chdir TMP_DIR do
+    stdout, _stderr, status = Open3.capture3(
       'texfot', 'pdftex', @hash
     )
-    error 400, _stdout unless status.success?
+    error 400, offset_line_numbers(stdout, -2).lines[2..-1].join unless status.success?
 
     _stdout, _stderr, status = Open3.capture3(
       'dvisvgm', '--no-fonts', @hash
@@ -36,5 +41,5 @@ post '/crank' do
   end
 
   content_type :svg
-  halt 200, File.read("#{CACHE_DIR}/#{@hash}.svg")
+  halt 200, File.read("#{TMP_DIR}/#{@hash}.svg")
 end
